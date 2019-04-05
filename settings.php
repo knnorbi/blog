ebe2b7d0c73029e7ed4a9f9302b948ebb03cc8ac
Norbert Kluka <kluka.norbert@gmail.com>
2019-04-05 13:07:38 +0200

    settings updated

--- settings.php
+++ settings.php
@@ -12,7 +12,7 @@ $userId = $_SESSION['id'];
 $hiba = false;
 $siker = false;
 
-if(isset($_POST['pass1']) && isset($_POST['pass2'])) {
+if(isset($_POST['pwchange'])) {
     $pass1 = $_POST['pass1'];
     $pass2 = $_POST['pass2'];
 
@@ -24,8 +24,19 @@ if(isset($_POST['pass1']) && isset($_POST['pass2'])) {
     }
     else {
         $password = md5($pass1);
-        $link->query("UPDATE users SET password = $password WHERE id = $userId;");
+        $link->query("UPDATE users SET password = '$password' WHERE id = $userId;");
         $siker = true;
+    }
+}
+
+if(isset($_POST['regdel'])) {
+    if(isset($_POST['tuti'])) {
+        $password = md5($_POST['pass']);
+        $result = $link->query("SELECT * FROM users WHERE password = '$password' AND id = $userId;");
+        if($result->num_rows == 1) {
+            $link->query("DELETE FROM users WHERE id = $userId;");
+            header("Location: logout.php");
+        }
     }
 }
 ?>
@@ -67,7 +78,7 @@ if(isset($_POST['pass1']) && isset($_POST['pass2'])) {
                     <input class="form-control" type="password" id="pass2" name="pass2">
                 </div>
                 <div class="form-group">
-                    <input class="btn btn-primary" type="submit" value="Jelszó változtatása">
+                    <input class="btn btn-primary" type="submit" value="Jelszó változtatása" name="pwchange">
                 </div>
             </form>
             <?php if($hiba) { print(file_get_contents("jelszohiba.php")); }?>
@@ -79,7 +90,20 @@ if(isset($_POST['pass1']) && isset($_POST['pass2'])) {
             <h3>Regisztráció törlése</h3>
         </div>
         <div class="col-md-8">
-            
+            <form action="settings.php" method="post">
+                <p>A regisztráció törléséhez kérlek add meg a jelszavadat!</p>
+                <div class="form-check">
+                    <input type="checkbox" id="tuti" name="tuti" value="igen" class="form-check-input">
+                    <label class="form-check-label" for="tuti">Biztosan törölni szeretném a regisztrációt!</label>
+                </div>
+                <div class="form-group">
+                    <label for="pass">Jelszó:</label>
+                    <input class="form-control" type="password" id="pass" name="pass">
+                </div>
+                <div class="form-group">
+                    <input class="btn btn-primary" type="submit" value="Törlés" name="regdel">
+                </div>
+            </form>
         </div>
     </div>
 </div>
