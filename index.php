f3137680aa983abdb346df440b20c822089d0988
Norbert Kluka <kluka.norbert@gmail.com>
2019-04-05 10:41:35 +0200

    filtering added

--- index.php
+++ index.php
@@ -3,6 +3,9 @@ session_start();
 if($_SESSION['level'] < 1) {
     header("Location: login.php");
 }
+
+include_once("dbcon.php");
+$link = DBcon();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -18,24 +21,47 @@ if($_SESSION['level'] < 1) {
 <form action="index.php" method="post">
 <div class="form-group">
 <label for="uzi">Message</label>
-<textarea class="form-control" name="uzi" id="uzi"></textarea><br>
+<textarea class="form-control" name="uzi" id="uzi" autofocus></textarea><br>
 </div>
 <input type="submit" class="btn btn-primary">
 </form>
-<a href="logout.php">Logout</a>
-<?php
+
+    <hr>
+    <h4>Üzenetek szűrése</h4>
+    <form method="get" action="index.php">
+        <div class="form-group">
+        <label for="SelectedUser">Felhasználó:</label>
+        <select class="form-control" id="SelectedUser" name="selectedUser">
+            <option value="0">Mindenki</option>
+            <?php
+            $result = $link->query("SELECT id, name FROM users;");
+            while ($row = mysqli_fetch_assoc($result)) {
+                $id = $row['id'];
+                $user = $row['name'];
+                echo "<option value=\"$id\"";
+                if(isset($_GET['selectedUser']) && $_GET['selectedUser'] == $id) {
+                    echo " selected";
+                }
+                echo ">$user</option>";
+            }
+            ?>
+        </select>
+        </div>
+        <div class="form-group">
+        <input type="submit" class="btn btn-primary" value="Szűrés">
+        </div>
+    </form>
+    <hr>
+    <a href="logout.php">Logout</a>
+    <?php
     if($_SESSION['level'] == 2) {
         echo "<br><a href=\"useradmin.php\">Felhasználók kezelése</a>";
     }
-?>
+    ?>
 </div>
 
 <div class="col-md-9">
 <?php
-
-include_once("dbcon.php");
-$link = DBcon();
-
 if(isset($_POST['uzi'])) {
     $uzenet = $_POST['uzi'];
     $datum = date('Y-m-d H:i:s');
@@ -44,8 +70,17 @@ if(isset($_POST['uzi'])) {
     //echo $query;
     $link->query($query);
 }
+$feltetel = "";
 
-$eredmeny = $link->query("SELECT uzik.id, uzi, name, date FROM uzik LEFT JOIN users ON users.id = uzik.user ORDER BY date;");
+if(isset($_GET['selectedUser']) && $_GET['selectedUser'] != 0) {
+    $feltetel = "WHERE users.id = " . $_GET['selectedUser'];
+}
+
+$eredmeny = $link->query(
+        "SELECT uzik.id, uzi, name, date 
+FROM uzik LEFT JOIN users ON users.id = uzik.user 
+$feltetel 
+ORDER BY date;");
 
 while ($row = mysqli_fetch_array($eredmeny)) {
     $uzi = $row['uzi'];
